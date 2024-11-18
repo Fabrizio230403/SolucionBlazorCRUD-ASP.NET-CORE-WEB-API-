a73431ea183cc892741f4cf70b52768ab1fa0b55
Piero <pfjimenez.29@gmail.com>
2024-11-18 13:54:58 -0500

    Permisos de Recurso

---
+++ BlazorCrud.Client/Services/IRecursoService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorCrud.Shared;
+namespace BlazorCrud.Client.Services
+{
+    public interface IRecursoService
+    {
+        Task<List<RecursoDTO>> Lista();
+        Task<RecursoDTO> Buscar(int id);
+        Task<int> Guardar(RecursoDTO recurso);
+        Task<int> Editar(RecursoDTO recurso);
+        Task<bool> Eliminar(int id);
+    }
+}
---
+++ BlazorCrud.Client/Services/RecursoService.cs
@@ -0,0 +1,68 @@
+﻿using BlazorCrud.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorCrud.Client.Services
+{
+    public class RecursoService : IRecursoService
+    {
+        private readonly HttpClient _http;
+
+        public RecursoService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<RecursoDTO>> Lista()
+        {
+            var result = await _http.GetFromJsonAsync<ResponseAPI<List<RecursoDTO>>>("api/Recurso/Lista");
+
+            if (result!.EsCorrecto)
+                return result.Valor!;
+            else
+                throw new Exception(result.Mensaje);
+        }
+
+        public async Task<RecursoDTO> Buscar(int id)
+        {
+            var result = await _http.GetFromJsonAsync<ResponseAPI<RecursoDTO>>($"api/Recurso/Buscar/{id}");
+
+            if (result!.EsCorrecto)
+                return result.Valor!;
+            else
+                throw new Exception(result.Mensaje);
+        }
+
+        public async Task<int> Guardar(RecursoDTO recurso)
+        {
+            var result = await _http.PostAsJsonAsync("api/Recurso/Guardar", recurso);
+            var response = await result.Content.ReadFromJsonAsync<ResponseAPI<int>>();
+
+            if (response!.EsCorrecto)
+                return response.Valor!;
+            else
+                throw new Exception(response.Mensaje);
+        }
+
+        public async Task<int> Editar(RecursoDTO recurso)
+        {
+            var result = await _http.PutAsJsonAsync($"api/Recurso/Editar/{recurso.RecursoId}", recurso);
+            var response = await result.Content.ReadFromJsonAsync<ResponseAPI<int>>();
+
+            if (response!.EsCorrecto)
+                return response.Valor!;
+            else
+                throw new Exception(response.Mensaje);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var result = await _http.DeleteAsync($"api/Recurso/Eliminar/{id}");
+            var response = await result.Content.ReadFromJsonAsync<ResponseAPI<int>>();
+
+            if (response!.EsCorrecto)
+                return response.EsCorrecto!;
+            else
+                throw new Exception(response.Mensaje);
+        }
+    }
+}
